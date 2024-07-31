b6aaa9b915adafad309d69255583b282d1bd45f6
Thiago Lahass <lahassthiagofelippe@gmail.com>
2024-07-30 22:31:46 -0300

    :fire:Cleaning code

--- T2_ThiagoFelippeNeitzkeLahass.py
+++ T2_ThiagoFelippeNeitzkeLahass.py
@@ -73,7 +73,6 @@ def carros(car_data, modelo=None, classe=None, ano=None):
         if (not modelo or car['model'] == modelo) and \
            (not classe or car['class'] == classe) and \
            (not ano or int(car['yearCar'][:4]) == ano):
-            #TO-DO: lembrar de mudar 'yearCar': car['yearCar'] para 'yearCar': int(car['yearCar'][:4]) caso seja conveniente
             carros_filtrados.append({'model': car['model'], 'class': car['class'], 'yearCar': car['yearCar'], 'manufacturer': car['manufacturer']})
     return carros_filtrados
 
@@ -95,7 +94,6 @@ def carros_busca(car_data, termo_busca, classe=None, ano=None):
             filtered_cars.append(car)
     return filtered_cars
 
-# ESTÁ CORRETO FAZER DESSA FORMA?
 def carros_fabricante(car_data, modelo=None, fabricante=None):
     carros_fabricantes = {car['model']: car['manufacturer'] for car in carros(car_data)}
 
@@ -106,7 +104,6 @@ def carros_fabricante(car_data, modelo=None, fabricante=None):
     else:
         return carros_fabricantes
 
-# É PERMITIDA ESSA IMPLEMENTAÇÃO ???????
 def fabricante_de_carros(car_data, fabricante=None, ano_fundacao=None, pais=None, area_servida=None):
     fabricantes = {car['manufacturer']: {'foundingYear': car['foundingYear'], 'manufacturerCountry': car['manufacturerCountry'], 'areaServed': car['areaServed']} for car in car_data}
     
@@ -139,7 +136,6 @@ def qtd_modelos_fabricante(car_data, fabricante=None):
         # Consulta fechada: retorna a quantidade de modelos do fabricante especificado
         return sum(1 for modelo, fab in carros_fabricantes.items() if fab == fabricante)
 
-# TO-DO: Verificar por que está dando alguns valores maiores aqui em comparação a quando aplica só "qtd_modelos_fabricante"
 # Função para contar a quantidade de modelos de fabricantes cujo ano de fundação seja na década de 1930
 def qtd_modelos_fabricante_30s(car_data, fabricante=None):
     carros_decada = [car for car in car_data if 1930 <= int(car['foundingYear'][:4]) < 1940]
@@ -304,25 +300,18 @@ CARS_DATA = getResults(RESULTS)
 # # 2. Consultas relacionadas a carros antigos:
 # # 2.1 Consulta aberta: todos os carros antigos
 # print(carro_antigo(CARS_DATA))
-#       MELHOR OPÇÃO ? (i.e aplicar a função "carros" antes de trabalhar os dados nessa função?)
-#       SE SIM, É MELHOR CHAMAR A FUNÇÃO "carros(CARS_DATA)" aqui ou na própria função "carros_antigos"?
-#       ACREDITO QUE DENTRO DA PRÓPIA FUNÇÃO, COMO ESTÁ IMPLEMENTADO ATÉ O MOMENTO
-# print(carro_antigo(carros(CARS_DATA)))
 # # 2.2 Consulta fechada: um carro antigo específico (exemplo: Fiat 1100 (True) e Lamborghini Aventador (False))
 # print(carro_antigo(CARS_DATA, 'Fiat 1100'))
 # print(carro_antigo(CARS_DATA, 'Lamborghini Aventador'))
 
 # # 3. Consultas relacionadas a busca de carros (consulta aberta não faz sentido aqui):
 # # 3.1 Consulta fechada: busca carros que contenham o termo da busca no nome do modelo (ex: 'Corolla')
-# print(carros_busca(carros(CARS_DATA), 'Corolla'))                           #QUAL DAS DUAS OPÇÕES É A MAIS CORRETA?
 # print(carros_busca(CARS_DATA, 'Corolla'))
 # # 3.2 Consulta fechada: Consulta por modelo "Lamborghini" lançado em 2020
-# print(carros_busca(carros(CARS_DATA), 'Lamborghini', ano=2020))             #QUAL DAS DUAS OPÇÕES É A MAIS CORRETA?
 # print(carros_busca(CARS_DATA, 'Lamborghini', ano=2020))
 
 # # 4. Consultas relacionadas a carros e seus respectivos fabricantes
 # # 4.1 Consulta aberta: Retorna todos os modelos e seus fabricantes
-# print(carros_fabricante(CARS_DATA))                                         #QUAL DAS DUAS OPÇÕES É A MAIS CORRETA?
 # # print(carros_fabricante(carros(CARS_DATA)))
 # # 4.2 Consulta semi-aberta: Retorna todos os modelos do fabricante especificado
 # print(carros_fabricante(CARS_DATA, fabricante='Fiat'))
@@ -378,9 +367,7 @@ CARS_DATA = getResults(RESULTS)
 
 # # 11. Consultas relacionadas a décadas de lançamento dos carros
 # # 11.1 Consulta aberta: todos os modelos com suas respectivas décadas de lançamento
-# # ==== MAIS UMA VEZ: QUAL DAS DUAS OPÇÕES É A MAIS CORRETA? ====
 # print(decada_de_lancamento_do_carro(CARS_DATA))
-# print(decada_de_lancamento_do_carro(carros(CARS_DATA)))
 # # 11.2 Consulta semi-aberta: todos os carros lançados na década especificada (ex: 2000)
 # print(decada_de_lancamento_do_carro(CARS_DATA, decada=2000))
 # # # 11.3 Consulta fechada: década de lançamento do modelo específico (ex: Toyota Corolla (E210))
@@ -401,233 +388,4 @@ CARS_DATA = getResults(RESULTS)
 
 # =======================================================================================
 # ====================================== CONSULTAS ======================================
-# =======================================================================================
-
-
-
-
-
-# =======================================================================================
-# =================== PROGRAMA EM PROLOG PARA COMPARAÇÕES E ANALISES ====================
-# =======================================================================================
-# :- data_source(
-# dbpedia_carros,
-# sparql("PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-# PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-# PREFIX dbo: <http://dbpedia.org/ontology/>
-# PREFIX dbow: <http://dbpedia.org/ontology/Work/>
-# PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-# PREFIX dbp: <http://dbpedia.org/property/>
-       
-# select distinct ?model ?yearCar ?class ?manufacturer ?foundingYear ?manufacturerCountry ?areaServed
-# 	where { 
-#         ?carro_uri a dbo:Automobile;
-#         	rdfs:label ?model;
-#         	dbo:productionStartYear ?productionStartYear;
-#         	dbo:class/rdfs:label ?class;
-#         	dbo:manufacturer ?manufacturer_uri.
-#         ?manufacturer_uri rdfs:label ?manufacturer;
-#         	dbo:foundingYear ?manufacturerFoundingYear;
-#         	dbp:locationCountry ?manufacturerCountry.
-#        		OPTIONAL {?manufacturer_uri dbp:areaServed ?areaServed}	
-#        BIND (xsd:gYear(?productionStartYear) as ?yearCar)
-#        BIND (xsd:gYear(?manufacturerFoundingYear) as ?foundingYear)
-#        FILTER (lang(?model) = 'en')
-#        FILTER (lang(?class) = 'en')
-#        FILTER (lang(?manufacturer) = 'en')
-#        FILTER (lang(?manufacturerCountry) = 'en')
-#        FILTER (lang(?areaServed) = 'en')
-#        }
-#        ORDER BY ASC(?year) ?model
-#   ",
-#   [ endpoint('https://dbpedia.org/sparql')])  ).
-
-# % Predicado para verificar se Substring faz parte de String
-# substring(String, Substring) :- sub_string(String, _, _, _, Substring).
-
-# % este predicado associa os carros às suas propriedades
-# carros(Modelo, Classe, AnoLancamento) :- distinct([Modelo, Classe],
-#                                          dbpedia_carros{model:Modelo, class:Classe, yearCar:AnoLancamento}).
-
-# % carros lancados antes de 1960
-# carroAntigo(Modelo) :- 	carros(Modelo, _, AnoDate),
-#     					date_time_value(date, DT, AnoDate),
-#     					date_time_value(year, DT, Ano),
-#     					Ano < 1960.
-
-# % busca um carro por parte do nome dele através do uso de string
-# carrosBusca(CarroBusca, Modelo, Classe, Ano) :- carros(Modelo, Classe, Ano),
-#     											substring(Modelo, CarroBusca).
-
-# % este predicado associa os modelos aos seus respectivos fabricantes
-# carrosFabricante(Modelo, Fabricante) :- distinct([Modelo],
-#                                        	dbpedia_carros{model:Modelo, manufacturer:Fabricante}).
-
-# % este predicado associa os fabricantes às suas propriedades
-# fabricanteDeCarros(Fabricante, AnoFundacao, Localizacao, AreaServida) :- 	distinct([Fabricante, Localizacao, AreaServida],
-#                                                                       		(dbpedia_carros{manufacturer:Fabricante, foundingYear:AnoFundacao, manufacturerCountry:Localizacao, areaServed:AreaServida})).
-
-# % retorna a quantidade de modelos fabricados pela fabricante
-# qtdModelosFabricante(Fabricante, QtdCarros) :- 	distinct([Fabricante], (fabricanteDeCarros(Fabricante,_,_,_))),
-#     											aggregate_all(count, (carrosFabricante(_, Fabricante)), QtdCarros).
-
-# % qtd de modelos de fabricantes cujo ano de fundação seja na decada de 1930
-# qtdModelosFabricante30s(Fabricante, QtdCarros) :-	fabricanteDeCarros(Fabricante, AnoFundacaoDate,_,_),
-#     												date_time_value(date, DT, AnoFundacaoDate),
-#     												date_time_value(year, DT, Ano),
-#     												Ano >= 1930, Ano < 1940,
-#     												aggregate_all(count, (carrosFabricante(_, Fabricante)), QtdCarros).
-    		
-# % fabricantes de carros que fornecem carros mundialmente
-# % se um fabricante não está servindo "Worldwide" diretamente, mas servir mais de 4 lugares diferentes,
-# % então ele também é classificado como vendedor mundial
-# fabricantesQueVendemMundialmente(Fabricante) :- distinct([Fabricante], (fabricanteDeCarros(Fabricante,_,_,AreaServida))),
-#     											substring(AreaServida, "Worldwide");
-#     											distinct([Fabricante], (fabricanteDeCarros(Fabricante,_,_,_))),
-#     											aggregate_all(count, (fabricanteDeCarros(Fabricante,_,_,_)), QtdAreas),
-#                                                 QtdAreas > 4.
-
-# % carros concorrentes
-# % verificar por que esta havendo repetição
-# carrosConcorrentes(Modelo1, Modelo2, Classe) :- carros(Modelo1, Classe, Ano1),
-#     											carros(Modelo2, Classe2, Ano2),
-#     											dif(Modelo1, Modelo2),
-#     											carrosFabricante(Modelo1, Fabricante1),
-#     											carrosFabricante(Modelo2, Fabricante2),
-#     											dif(Fabricante1, Fabricante2),
-#     											Classe2 == Classe,
-#     											date_time_value(date, DT, Ano1),
-#     											date_time_value(year, DT, AnoModelo1),
-#     											date_time_value(date, DT, Ano2),
-#     											date_time_value(year, DT, AnoModelo2),
-#     											AnoModelo1 == AnoModelo2.
-
-# % carro confiável seria aquele cujo fabricante têm uma ampla área de serviço e está no mercado há mais de 20 anos
-# % e não é um carro antigo
-# carroConfiavel(Modelo) :- distinct([Modelo], (carrosFabricante(Modelo, Fabricante))),
-#                           fabricanteDeCarros(Fabricante, AnoFundacao, _, _),
-#                           date_time_value(date, DT, AnoFundacao),
-#     					    date_time_value(year, DT, Ano),
-#                           Ano < 2004,
-#                           fabricantesQueVendemMundialmente(Fabricante),
-#     					  \+carroAntigo(Modelo).
-
-# decadaDeLancamentoDoCarro(Modelo, 1940) :- 	distinct([Modelo], (carros(Modelo, _, Ano))),
-#     										date_time_value(date, DT, Ano),
-#     									 	date_time_value(year, DT, AnoNum),
-#     										AnoNum < 1950.
-# decadaDeLancamentoDoCarro(Modelo, 1950) :- 	distinct([Modelo], (carros(Modelo, _, Ano))),
-#     										date_time_value(date, DT, Ano),
-#     									 	date_time_value(year, DT, AnoNum),
-#     										AnoNum >= 1950,
-#     										AnoNum < 1960.
-# decadaDeLancamentoDoCarro(Modelo, 1960) :- 	distinct([Modelo], (carros(Modelo, _, Ano))),
-#     										date_time_value(date, DT, Ano),
-#     									 	date_time_value(year, DT, AnoNum),
-#     										AnoNum >= 1960,
-#     										AnoNum < 1970.
-# decadaDeLancamentoDoCarro(Modelo, 1970) :- 	distinct([Modelo], (carros(Modelo, _, Ano))),
-#     										date_time_value(date, DT, Ano),
-#     									 	date_time_value(year, DT, AnoNum),
-#     										AnoNum >= 1970,
-#     										AnoNum < 1980.
-# decadaDeLancamentoDoCarro(Modelo, 1980) :- 	distinct([Modelo], (carros(Modelo, _, Ano))),
-#     										date_time_value(date, DT, Ano),
-#     									 	date_time_value(year, DT, AnoNum),
-#     										AnoNum >= 1980,
-#     										AnoNum < 1990.
-# decadaDeLancamentoDoCarro(Modelo, 1990) :- 	distinct([Modelo], (carros(Modelo, _, Ano))),
-#     										date_time_value(date, DT, Ano),
-#     									 	date_time_value(year, DT, AnoNum),
-#     										AnoNum >= 1990,
-#     										AnoNum < 2000.
-# decadaDeLancamentoDoCarro(Modelo, 2000) :- 	distinct([Modelo], (carros(Modelo, _, Ano))),
-#     										date_time_value(date, DT, Ano),
-#     									 	date_time_value(year, DT, AnoNum),
-#     										AnoNum >= 2000,
-#     										AnoNum < 2010.
-# decadaDeLancamentoDoCarro(Modelo, 2010) :- 	distinct([Modelo], (carros(Modelo, _, Ano))),
-#     										date_time_value(date, DT, Ano),
-#     									 	date_time_value(year, DT, AnoNum),
-#     										AnoNum >= 2010,
-#     										AnoNum < 2020.
-# decadaDeLancamentoDoCarro(Modelo, 2020) :- 	distinct([Modelo], (carros(Modelo, _, Ano))),
-#     										date_time_value(date, DT, Ano),
-#     									 	date_time_value(year, DT, AnoNum),
-#     										AnoNum >= 2020.
-
-# % se então (->)
-# % verificar se determinado modelo foi lançado no ultimo ano
-# novoModelo(Modelo) :-  	distinct([Modelo], carros(Modelo, _, AnoLancamento)),
-#     					date_time_value(date, DT, AnoLancamento),
-#     					date_time_value(year, DT, AnoNum),
-#     					(/*if*/ AnoNum == 2024
-#                        	/*then*/ -> true;
-#                         /*else*/ false).
-
-# % quantidade de fabricantes por país
-# qtdFabricantesPorPais(Pais, QtdFabricantes) :- 	distinct([Pais], (fabricanteDeCarros(_,_,Pais,_))),
-#     											aggregate_all(count, (fabricanteDeCarros( _, _, Pais, _)), QtdFabricantes).
-
-
-# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-# %%%%%%%%%%%% CONSULTAS PRE-DEFINIDAS %%%%%%%%%%%%%%%
-# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-# /** <examples>
-
-# ?- carros(Modelo, Classe, Ano).
-# ?- carros(Modelo, Classe, date(2023,1,1)).
-# ?- carros("Lamborghini Aventador", Classe, Ano).
-
-# ?- carroAntigo(Modelo).
-# ?- carroAntigo("Fiat 1100").
-# ?- carroAntigo("Toyota Corolla (E210)").
-
-# ?- carrosBusca("Corolla", Modelo, Classe, Ano).
-# ?- carrosBusca("Lamborghini", Modelo, Classe, date(2020,1,1)).
-
-# ?- carrosFabricante(Modelo, Fabricante).
-# ?- carrosFabricante(Modelo, "Fiat").
-# ?- carrosFabricante("Cadillac Celestiq", Fabricante).
-
-# ?- fabricanteDeCarros(Fabricante, AnoFundacao, Localizacao, AreaServida).
-# ?- fabricanteDeCarros(Fabricante, date(2000,1,1), Localizacao, AreaServida).
-# ?- fabricanteDeCarros(Fabricante, date(2000,1,1), "Japan", AreaServida).
-
-# ?- qtdModelosFabricante(Fabricante, QtdCarros).
-# ?- qtdModelosFabricante("Toyota", QtdCarros).
-
-# ?- qtdModelosFabricante30s(Fabricante, QtdCarros).
-# ?- qtdModelosFabricante30s("Volkswagen", QtdCarros).
-# ?- qtdModelosFabricante30s("BYD Auto", QtdCarros). %retorna falso por que essa nao é uma fabricante fundada e
-# na década de 30
-
-# ?- fabricantesQueVendemMundialmente(Fabricante).
-# ?- fabricantesQueVendemMundialmente("BMW").
-# ?- fabricantesQueVendemMundialmente("Arcfox").
-# ?- fabricantesQueVendemMundialmente("Chevrolet"). % não serve "Worldwide", mas serve mais de 4 lugares diferentes, e retorna 'true'
-
-# ?- carrosConcorrentes(Modelo1, Modelo2, Classe).
-# ?- carrosConcorrentes("Toyota Corolla (E210)", Modelo, Classe).
-# ?- carrosConcorrentes("BYD e2", Modelo, Classe).
-# ?- carrosConcorrentes(Modelo1, Modelo2, "Compact car").
-
-# ?- carroConfiavel(Modelo).
-# ?- carroConfiavel("Toyota RAV4").
-
-# ?- decadaDeLancamentoDoCarro(Modelo, Decada).
-# ?- decadaDeLancamentoDoCarro("Toyota RAV4", Decada).
-# ?- decadaDeLancamentoDoCarro(Modelo, 2000).
-
-# ?- novoModelo(Modelo).
-# ?- novoModelo("Toyota RAV4").
-# ?- novoModelo("Cadillac Celestiq").
-
-# ?- qtdFabricantesPorPais(Pais, QtdFabricantes).
-# ?- qtdFabricantesPorPais("Japan", QtdFabricantes).
-
-# */
-
-# =======================================================================================
-# =================== PROGRAMA EM PROLOG PARA COMPARAÇÕES E ANALISES ====================
 # =======================================================================================
